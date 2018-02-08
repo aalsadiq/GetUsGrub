0e457738ec5a19215fb395fe38753a258c7b53ec
Andrew Kao <kao81093@gmail.com>
2018-02-08 03:14:03 -0800

    Profile Management Models
    + Models (IProfile, IRestaurantProfile, RegularProfile, RestaurantMenuItem, BusinessHour)
    
    RestaurantMenu and RestaurantProfile classes still WIP

---
+++ GitGrub.GetUsGrub/GitGrub.GetUsGrub/Models/IProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitGrub.GetUsGrub.Models
+{
+    interface IProfile
+    {
+        string GetProfileName();
+        string GetProfilePicture();
+    }
+}

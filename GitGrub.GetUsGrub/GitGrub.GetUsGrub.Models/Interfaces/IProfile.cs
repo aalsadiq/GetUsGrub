c3ff786564e21d3f03343a187d40d27accdee193
Andrew Kao <kao81093@gmail.com>
2018-02-13 15:12:01 -0800

    Profile Management Interfaces and Models
    Interfaces
    + IProfile
    + IRestaurantProfile
    
    Models
    +BusinessHours
    +RegularProfile
    +RestaurantMenuItem
    +RestaurantProfile

---
+++ GitGrub.GetUsGrub/GitGrub.GetUsGrub.Models/Interfaces/IProfile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitGrub.GetUsGrub.Models
+{
+    interface IProfile
+    {
+        string ProfileName { get; set; }
+        string ProfilePicture { get; set; }
+    }
+}
---
+++ GitGrub.GetUsGrub/GitGrub.GetUsGrub.Models/Models/RestaurantProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitGrub.GetUsGrub.Models.Models
+{
+    class RestaurantProfile : IProfile, IRestaurantProfile
+    {
+        public string ProfileName { get; set; }
+        public string ProfilePicture { get; set; }
+        public string Category { get; set; }
+        public IEnumerable<BusinessHours> BusinessSchedule { get; set; }
+        public bool Reservations { get; set; }
+        public bool Delivery { get; set; }
+        public bool TakeOut { get; set; }
+        public bool AcceptCreditCards { get; set; }
+        public string Attire { get; set; }
+        public bool ServesAlcohol { get; set; }
+        public bool OutdoorSeating { get; set; }
+        public bool HasTV { get; set; }
+        public bool DriveThru { get; set; }
+        public bool Caters { get; set; }
+        public bool Pet { get; set; }
+        public IList<RestaurantMenuItem> Menu { get; set; }
+    }
+}

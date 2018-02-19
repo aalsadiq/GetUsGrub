c0ab001247b0d7fbddc13950fd7694bfd3c323d2
Unknown <Jennifer.Nguyen27@student.csulb.edu>
2018-02-18 18:32:09 -0800

    Added Security Question class and its list interface
    [+] SecurityQuestion.cs
    [+] ISecurityQuestionsList.cs

---
+++ GitGrub.GetUsGrub/GitGrub.GetUsGrub.Models/Interfaces/ISecurityQuestionsList.cs
@@ -0,0 +1,10 @@
+﻿using GitGrub.GetUsGrub.Models.Models;
+using System.Collections.Generic;
+
+namespace GitGrub.GetUsGrub.Models.Interfaces
+{
+    public interface ISecurityQuestionsList
+    {
+        IEnumerable<SecurityQuestion> SecurityQuestionsList { get; set; }
+    }
+}

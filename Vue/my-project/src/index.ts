c13d48aa385c76ab28218ff090223ffc2c2cbe80
Ryan L <luong_psn@yahoo.com>
2018-02-19 19:22:54 -0800

    Merge branch 'Ryan' into remote-tracking branch 'origin/master'
    Most up-to-date Vue, with TypeScript and BootStrap capabilities. Note: BootStrap syntax will not show up automatically when you type in your IDE, so refer to https://uiv.wxsm.space/ for useful syntax.

--- Vue/my-project/src/index.ts
+++ Vue/my-project/src/index.ts
@@ -1,65 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//import '../static/bootstrap-3.3.7-dist/css/bootstrap.min.css';
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import App from './App.vue';
-import Home from "./components/Home.vue"
-import Login from "./components/Login.vue"
-import RestaurantBillSplitter from "./components/RestaurantBillSplitter/RestaurantBillSplitter.vue"
-// import './bootstrap-3.3.7/dist/css/bootstrap.min.css';
-// import draggable from 'vuedraggable'
+import Routes from "./router/routes.js";
+//import BootstrapVue from 'bootstrap-vue'
+import * as uiv from 'uiv';
 
-//Vue.component('draggable', draggable) // Allows usage of Vue Draggable across the entire application
 
 /* eslint-disable no-new */
 export const EventBus = new Vue();
+Vue.use(uiv);
+Vue.use(VueRouter);
+//Vue.use(BootstrapVue);
 
 const router = new VueRouter({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-      beforeEnter: (to, from, next) => {
-        document.title = 'Home'
-        next()
-      }
-    },
-    {
-      path: '/Login',
-      name: 'Login',
-      component: Login,
-      beforeEnter: (to, from, next) => {
-        document.title = 'Login'
-        next()
-      }/*,
-      beforeRouteLeave: (to, from, next) => {
-        if (to.path === 'SignIn') {
-          next(false)
-        }
-      }*/
-    },
-    {
-      path: '/RestaurantBillSplitter',
-      name: 'RestaurantBillSplitter',
-      component: RestaurantBillSplitter,
-      beforeEnter: (to, from, next) => {
-        document.title = 'Split Your Bill!'
-        next()
-      }
-    },
-    {
-      path: '*'
-    }
-    ]
+  routes: Routes
 });
 
-Vue.use(VueRouter);
+
 const app = new Vue({
   el: '#app',
-  router,
+  router: router,
   components: {
-    App//, draggable
+    App
   },
-  template: '<router-view></router-view>'
+  template: '<App/>'
 })

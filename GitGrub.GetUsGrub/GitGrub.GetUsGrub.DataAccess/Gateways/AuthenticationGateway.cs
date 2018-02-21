0405fbbd230c7c2759ac39095cfb49d044468e93
fannbrian <fannbrian@gmail.com>
2018-02-20 17:20:29 -0800

    Created empty auth gateway and interface

--- GitGrub.GetUsGrub/GitGrub.GetUsGrub.DataAccess/Gateways/AuthenticationGateway.cs
+++ GitGrub.GetUsGrub/GitGrub.GetUsGrub.DataAccess/Gateways/AuthenticationGateway.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace GitGrub.GetUsGrub.DataAccess.Gateways
+namespace GitGrub.GetUsGrub.DataAccess
 {
-    public class AuthenticationGateway
+    public class AuthenticationGateway : IAuthenticationGateway
     {
     }
 }

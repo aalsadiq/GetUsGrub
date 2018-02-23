ef8fb4ad94eb8cdb4046f9090336f4e750621ce9
Unknown <angelica04salas@gmail.com>
2018-02-22 18:40:25 -0800

    update

---
+++ GitGrub.GetUsGrub/GitGrub.GetUsGrub.Models/Models/RegularProfile.cs
@@ -0,0 +1,8 @@
+﻿namespace GitGrub.GetUsGrub.Models
+{
+    public class RegularProfile : IProfile
+    {
+        public string ProfileName { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}

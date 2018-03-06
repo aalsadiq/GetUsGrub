bec0adaefb758fdb6e19991566df36ac3c6de316
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-06 11:04:45 -0800

    Added new vue project

--- get-us-grub/src/App.vue
+++ get-us-grub/src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <v-app>
+    <app-header/>
+    <!-- <img src="./assets/logo.png"> -->
     <router-view/>
+    <app-footer/>
+    </v-app>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader'
+import AppFooter from './components/AppFooter'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {AppHeader,AppFooter}
 }
 </script>
 
@@ -18,6 +26,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ get-us-grub/src/components/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-footer height="auto" class="grey darken-3" fixed>
+    <v-layout row wrap justify-center>
+      <v-flex xs12 py-3 text-xs-center white--text>
+          &copy; {{ new Date().getFullYear() }} — <strong>GitGrub</strong>
+      </v-flex>
+    </v-layout>
+  </v-footer>
+</template>

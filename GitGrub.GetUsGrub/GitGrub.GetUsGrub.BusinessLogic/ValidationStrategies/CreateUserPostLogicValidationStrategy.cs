61b6dd96612e91168d3a8bd7a9235f631876c2af
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-06 17:26:34 -0800

    Completed Validation for Individual Users
    RegisterUserPostLogicValidatorStrategy
    PasswordSaltValidator
    SecurityAnswerSaltValidator
    ClaimsValidator

--- GitGrub.GetUsGrub/GitGrub.GetUsGrub.BusinessLogic/ValidationStrategies/CreateUserPostLogicValidationStrategy.cs
+++ GitGrub.GetUsGrub/GitGrub.GetUsGrub.BusinessLogic/ValidationStrategies/CreateUserPostLogicValidationStrategy.cs
@@ -10,28 +10,27 @@ namespace GitGrub.GetUsGrub.BusinessLogic
     /// Returns a general error if failure occurs.
     /// <para>
     /// @author: Jennifer Nguyen
-    /// @updated: 03/05/2018
+    /// @updated: 03/06/2018
     /// </para>
     /// </summary>
     public class CreateUserPostLogicValidationStrategy : IValidationStrategy<IRegisterUserDto>
     {
-        // TODO: Strategy Pattern
         public ResponseDto<IRegisterUserDto> RunValidators(IRegisterUserDto registerUserDto)
         {
             var responseDto = new ResponseDto<IRegisterUserDto>() {Data = registerUserDto};
 
-            var userAccountValidator = new UserAccountValidator();
-
             // Validate UserAccount
+            var userAccountValidator = new UserAccountValidator();
             var validationResult = userAccountValidator.Validate(registerUserDto.UserAccount, ruleSet: "RegistrationPostLogic");
-
+            // TODO: Make this part of a Validation service to check if result is valid
             if (!validationResult.IsValid)
             {
                 responseDto.Error = ErrorHandler.GetGeneralError();
+                return responseDto;
             }
 
-            var securityQuestionValidator = new SecurityQuestionValidator();
             // Validate SecurityQuestions
+            var securityQuestionValidator = new SecurityQuestionValidator();
             foreach (var securityQuestion in responseDto.Data.SecurityQuestions)
             {
                 validationResult = securityQuestionValidator.Validate(securityQuestion, ruleSet: "RegistrationPostLogic");
@@ -39,11 +38,34 @@ namespace GitGrub.GetUsGrub.BusinessLogic
                 responseDto.Error = ErrorHandler.GetGeneralError();
                 return responseDto;
             }
+            
+            // Validate SecurityAnswerSalts
+            var securityAnswerSaltValidator = new SecurityAnswerSaltValidator();
+            foreach (var securityAnswerSalt in responseDto.Data.SecurityAnswerSalts)
+            {
+                validationResult = securityAnswerSaltValidator.Validate(securityAnswerSalt);
+                if (validationResult.IsValid) continue;
+                responseDto.Error = ErrorHandler.GetGeneralError();
+                return responseDto;
+            }
 
-            // TODO: Security Answers
-            // TODO: PasswordSalt
-            // TODO: Claims
+            // Validate PasswordSalt
+            var passwordSaltValidator = new PasswordSaltValidator();
+            validationResult = passwordSaltValidator.Validate(responseDto.Data.PasswordSalt);
+            if (!validationResult.IsValid)
+            {
+                responseDto.Error = ErrorHandler.GetGeneralError();
+                return responseDto;
+            }
 
+            // Validate Claims
+            var claimsValidator = new ClaimsValidator();
+            validationResult = claimsValidator.Validate(responseDto.Data.Claims);
+            if (!validationResult.IsValid)
+            {
+                responseDto.Error = ErrorHandler.GetGeneralError();
+                return responseDto;
+            }
             return responseDto;
         }
     }

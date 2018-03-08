e23af33f330b1e1308ea22563d984360b1d69549
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-07 21:49:55 -0800

    Adding Registration.Vue
    Included finished form for general users and unfinished form for restaurant users.
    Transferring files from my Vue branch. May not use it anymore.

---
+++ get-us-grub/get-us-grub/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <v-app>
+      <app-header/>
+      <v-container id="router-container">
+        <div>
+            <!-- <img src="./assets/logo.png"> -->
+            <router-view/>
+        </div>
+      </v-container>
+      <app-footer/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader'
+import AppFooter from './components/AppFooter'
+
+export default {
+  name: 'App',
+  components: {AppHeader, AppFooter}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+#router-container {
+  margin-top: 4.5em;
+}
+</style>

214716f8d99e44d49b34979b90560bb8ea7e7b40
Rachel <rachel.dang@student.csulb.edu>
2018-03-10 14:36:13 -0800

    Jenn's Demo

---
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.DataAccess/Contexts/RestaurantContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace GitGrub.GetUsGrub.DataAccess
+{
+    /// <summary>
+    /// Context used for food selection + restaurant user profile management.
+    /// Context that allows connections to specific entities for restaurant selection and restaurant profile management.
+    ///
+    /// Brian Fann
+    /// 2/21/18
+    /// </summary>
+    public class RestaurantContext : DbContext
+    {
+        public RestaurantContext() : base("GetUsGrub") { }
+    }
+
+}

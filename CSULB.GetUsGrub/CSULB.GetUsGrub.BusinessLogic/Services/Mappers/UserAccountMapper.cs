595083007d1cfc143ec723c0eea9dbabd4a3b8b0
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-11 17:53:30 -0700

    Adding Mappers
    [+] RestaurantProfileMapper
    [+] SecurityQuestionMapper
    [+] UserAccountMapper
    [+] UserProfileMapper

---
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.BusinessLogic/Services/Mappers/UserAccountMapper.cs
@@ -0,0 +1,35 @@
+﻿using CSULB.GetUsGrub.Models;
+
+namespace CSULB.GetUsGrub.BusinessLogic
+{
+    /// <summary>
+    /// The <c>UserAccountMapper</c> class.
+    /// Maps data transfer objects to and from UserAccount model.
+    /// <para>
+    /// @author: Jennifer Nguyen
+    /// @updated: 03/11/2018
+    /// </para>
+    /// </summary>
+    public class UserAccountMapper
+    {
+        public UserAccountDto ModelToDto(UserAccount userAccount)
+        {
+            var userAccountDto = new UserAccountDto
+            {
+                Username = userAccount.Username,
+                Password = userAccount.Password
+            };
+            return userAccountDto;
+        }
+
+        public UserAccount DtoToModel(UserAccountDto userAccountDto)
+        {
+            var userAccount = new UserAccount
+            {
+                Username = userAccountDto.Username,
+                Password = userAccountDto.Password
+            };
+            return userAccount;
+        }
+    }
+}

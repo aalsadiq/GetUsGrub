f937c9864780d3f414addabb4e9b044e42c77f1a
Rachel <rachel.dang@student.csulb.edu>
2018-03-13 18:14:54 -0700

    Added filter to Web Api Config and fixed namespaces in User Access Control

--- CSULB.GetUsGrub/CSULB.GetUsGrub/App_Start/WebApiConfig.cs
+++ CSULB.GetUsGrub/CSULB.GetUsGrub/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using CSULB.GetUsGrub.UserAccessControl;
+using System.Web.Http;
 
 namespace CSULB.GetUsGrub
 {
@@ -17,6 +18,9 @@ namespace CSULB.GetUsGrub
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Add GlobalSecurityExceptionFilter for User Access Control
+            config.Filters.Add(new GlobalSecurityExceptionFilter());
         }
     }
 }

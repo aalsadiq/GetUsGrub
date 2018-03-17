da3aa1ef2d741d241060c852c39f05c36479fbdf
Ryan L <luong_psn@yahoo.com>
2018-03-17 14:52:15 -0700

    Restaurant Bill Splitter Updates
    +Installed V-Money
    +Changes things in the store
    +Uninstalled vue-axios

--- get-us-grub/src/store/store.js
+++ get-us-grub/src/store/store.js
@@ -5,52 +5,42 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-    MINIMUM_MENU_ITEM_PRICE: 0.01,
-    MAX_MENU_ITEM_PRICE: 1000,
     MenuItems: [
-      {
-        menuItemName: 'Big Mac',
-        menuItemPrice: 4.00
-      },
-      {
-        menuItemName: 'Large Fries',
-        menuItemPrice: 2.50
-      },
-      {
-        menuItemName: 'McFlurry',
-        menuItemPrice: 1.00
-      },
-      {
-        menuItemName: 'Large Soft Drink',
-        menuItemPrice: 2.00
-      },
-      {
-        menuItemName: 'McChicken',
-        menuItemPrice: 1.50
-      },
-      {
-        menuItemName: 'Water Bottle',
-        menuItemPrice: 10.50
-      }
     ],
     BillItems: [
+      {
+      menuItemName: 'Test',
+      menuItemPrice: 2.00,
+      menuItemEdit: false
+      }
     ]
   },
   getters: {
-    // totalPrice: state => {
-    //  var totalPrice = this.$store.state.BillItems.map(BillItems => {
-    //    return state.BillItems.menuItemPrice
-    //  })
-    // }
+    totalPrice: state => {
+      var temp = 0
+      state.BillItems.forEach(function (element) {
+        temp += element.menuItemPrice
+      })
+      return temp
+     }
   },
   mutations: {
     AddToDictionary: (state, payload) => {
       state.MenuItems.push({
         menuItemName: payload[0],
-        menuItemPrice: payload[1]
+        menuItemPrice: payload[1],
+        menuItemEdit: false
       })
     },
-
+    ToggleEdit: (state, payload) => {
+      var temp = state.MenuItems[payload].menuItemEdit
+      state.MenuItems.forEach(function (element) {
+        element.menuItemEdit = false
+      })
+      if (temp == false) {
+        state.MenuItems[payload].menuItemEdit = true
+      }
+    },
     RemoveFromDictionary: (state, payload) => {
       console.log('Store Mutation: ' + payload)
       state.MenuItems.splice(payload, 1)
@@ -68,6 +58,11 @@ export const store = new Vuex.Store({
         console.log('Added Food Item Price: ' + payload[1])
         context.commit('AddToDictionary', payload)
       }, 500)
+    },
+    ToggleEdit: (context, payload) => {
+      setTimeout(function () {
+        context.commit('ToggleEdit', payload)
+      }, 250)
     },
     RemoveFromDictionary: (context, payload) => {
       setTimeout(function () {

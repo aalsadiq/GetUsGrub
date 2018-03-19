e9ae461897a669ab02f9630e39e28c5abac0a491
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-18 21:16:28 -0700

    Updating changes due to change in handling errors

--- CSULB.GetUsGrub/CSULB.GetUsGrub.BusinessLogic/Mappers/UserAccountMapper.cs
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.BusinessLogic/Mappers/UserAccountMapper.cs
@@ -1,39 +1,39 @@
-﻿using CSULB.GetUsGrub.Models;
+﻿//using CSULB.GetUsGrub.Models;
 
-namespace CSULB.GetUsGrub.BusinessLogic
-{
-    /// <summary>
-    /// The <c>UserAccountMapper</c> class.
-    /// Maps data transfer objects to and from UserAccount model.
-    /// <para>
-    /// @author: Jennifer Nguyen
-    /// @updated: 03/11/2018
-    /// </para>
-    /// </summary>
-    public class UserAccountMapper
-    {
-        public UserAccountDto MapModelToDto(UserAccount userAccount)
-        {
-            var userAccountDto = new UserAccountDto
-            {
-                Username = userAccount.Username,
-                Password = userAccount.Password,
-                IsActive = userAccount.IsActive,
-                IsFirstTimeUser = userAccount.IsFirstTimeUser
-            };
-            return userAccountDto;
-        }
+//namespace CSULB.GetUsGrub.BusinessLogic
+//{
+//    /// <summary>
+//    /// The <c>UserAccountMapper</c> class.
+//    /// Maps data transfer objects to and from UserAccount model.
+//    /// <para>
+//    /// @author: Jennifer Nguyen
+//    /// @updated: 03/11/2018
+//    /// </para>
+//    /// </summary>
+//    public class UserAccountMapper
+//    {
+//        public UserAccountDto MapModelToDto(UserAccount userAccount)
+//        {
+//            var userAccountDto = new UserAccountDto
+//            {
+//                Username = userAccount.Username,
+//                Password = userAccount.Password,
+//                IsActive = userAccount.IsActive,
+//                IsFirstTimeUser = userAccount.IsFirstTimeUser
+//            };
+//            return userAccountDto;
+//        }
 
-        public UserAccount MapDtoToModel(UserAccountDto userAccountDto)
-        {
-            var userAccount = new UserAccount
-            {
-                Username = userAccountDto.Username,
-                Password = userAccountDto.Password,
-                IsActive = userAccountDto.IsActive,
-                IsFirstTimeUser = userAccountDto.IsFirstTimeUser
-            };
-            return userAccount;
-        }
-    }
-}
+//        public UserAccount MapDtoToModel(UserAccountDto userAccountDto)
+//        {
+//            var userAccount = new UserAccount
+//            {
+//                Username = userAccountDto.Username,
+//                Password = userAccountDto.Password,
+//                IsActive = userAccountDto.IsActive,
+//                IsFirstTimeUser = userAccountDto.IsFirstTimeUser
+//            };
+//            return userAccount;
+//        }
+//    }
+//}

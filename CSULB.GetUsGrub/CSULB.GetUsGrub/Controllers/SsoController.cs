9ee0882472d7d4c4c4353fc6ab472ef0643362ef
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-19 15:01:05 -0700

    Updating SsoController
    Included call to TokenService and creating UserAccount in database

--- CSULB.GetUsGrub/CSULB.GetUsGrub/Controllers/SsoController.cs
+++ CSULB.GetUsGrub/CSULB.GetUsGrub/Controllers/SsoController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CSULB.GetUsGrub.BusinessLogic;
+using CSULB.GetUsGrub.Models;
+using System;
 using System.Net.Http;
 using System.Web.Http;
 
@@ -8,24 +10,45 @@ namespace CSULB.GetUsGrub
     public class SsoController : ApiController
     {
         [HttpPost]
-        [Route("Registration/Individual")]
+        [Route("Registration")]
+        // TODO: @Jenn Update origins to reflect SSO request [-Jenn]
         //[EnableCors(origins: "http://localhost:8080", headers: "*", methods: "POST")]
         public IHttpActionResult RegisterFirstTimeSsoUser(HttpRequestMessage request)
         {
+            var userManager = new UserManager();
+
             try
             {
-                var requestHeaders = request.Headers;
-                if (requestHeaders.Contains("Bearer"))
+                ResponseDto<UserAccountDto> tokenResult;
+
+                // TODO: @Jenn Check for if message has a token. Where to put this? [-Jenn]
+                if (request.Headers.Authorization == null)
                 {
-                    return Ok();
+                    return BadRequest("Request does not contain a token.");
                 }
+
+                using (var tokenService = new TokenService())
+                {
+                   tokenResult = tokenService.ValidateToken(request.Headers.Authorization.Parameter);
+                }
+
+                if (tokenResult.Error != null)
+                {
+                    return BadRequest(tokenResult.Error);
+                }
+
+                var result = userManager.CreateFirstTimeSsoUser(tokenResult.Data);
+                if (result.Error != null)
+                {
+                    return BadRequest(result.Error);
+                }
+
+                return Ok(result.Data);
             }
             catch (Exception)
             {
-                return BadRequest();
+                return BadRequest("Something went wrong. Please try again later.");
             }
-
-            return Ok();
         }
     }
 }

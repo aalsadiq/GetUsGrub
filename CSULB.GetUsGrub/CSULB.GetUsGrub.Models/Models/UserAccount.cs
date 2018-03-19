70130dcf2d8ec9e12b8adf149800e59a0b817721
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-18 21:14:15 -0700

    Adding in constructors to the UserAccount models and DTOs

--- CSULB.GetUsGrub/CSULB.GetUsGrub.Models/Models/UserAccount.cs
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.Models/Models/UserAccount.cs
@@ -15,14 +15,44 @@ namespace CSULB.GetUsGrub.Models
     [Table("GetUsGrub.UserAccount")]
     public class UserAccount : IUserAccount, IEntity
     {
+        public UserAccount(string username)
+        {
+            Username = username;
+        }
+
+        public UserAccount(string username, string password, bool isFirstTimeUser, string roleType)
+        {
+            Username = username;
+            Password = password;
+            IsFirstTimeUser = isFirstTimeUser;
+            RoleType = roleType;
+        }
+
+        public UserAccount(string username, string password, bool isActive, bool isFirstTimeUser)
+        {
+            Username = username;
+            Password = password;
+            IsActive = isActive;
+            IsFirstTimeUser = isFirstTimeUser;
+        }
+
+        public UserAccount(UserAccountDto userAccountDto)
+        {
+            Username = userAccountDto.Username;
+            IsActive = userAccountDto.IsActive;
+            IsFirstTimeUser = userAccountDto.IsFirstTimeUser;
+        }
+
         [Key]
         public int? Id { get; set; }
         public string Username { get; set; }
 
         // Stored as a hash
         public string Password { get; set; }
-        public bool IsActive { get; set; }
-        public bool IsFirstTimeUser { get; set; }
+        public bool? IsActive { get; set; }
+        public bool? IsFirstTimeUser { get; set; }
+        // Public or private roles
+        public string RoleType { get; set; }
 
         // Navigation Properties
         public virtual UserProfile UserProfile { get; set; }

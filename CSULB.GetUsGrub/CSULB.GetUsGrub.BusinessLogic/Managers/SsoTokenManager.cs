50814c768b61195db46c8d018738a32d388ca9cc
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-20 12:51:50 -0700

    Updated Sso Validation Strategies
    Not yet finished since I am still waiting for token service

---
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.BusinessLogic/Managers/SsoTokenManager.cs
@@ -0,0 +1,105 @@
+﻿using CSULB.GetUsGrub.DataAccess;
+using CSULB.GetUsGrub.Models;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+// TODO: @Jenn compare token to database. Add token to database [-Jenn]
+namespace CSULB.GetUsGrub.BusinessLogic
+{
+    public class SsoTokenManager : IDisposable
+    {
+        private readonly SecurityKey _securityKey;
+        private readonly TokenService _tokenService;
+
+        public SsoTokenManager()
+        {
+
+            _securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw=="));
+            _tokenService = new TokenService();
+        }
+
+        // TODO: @Jenn Should we send back a ResponseDto? May need to encapsulate some procedures in reusable methods [-Jenn]
+        public ResponseDto<UserAccountDto> ValidateToken(string token)
+        {
+            string username = "";
+            string password = "";
+            string roleType = "";
+    
+            if (!_tokenService.CheckIfTokenIsJsonWebToken(token))
+            {
+                return new ResponseDto<UserAccountDto>()
+                {
+                    Error = "The token is not a well formed Json Web Token."
+                };
+            }
+
+            var jwt = _tokenService.GetJwtSecurityToken(token);
+            var header = jwt.Header;
+            var payload = jwt.Payload;
+            var rawSignature = jwt.RawSignature;
+
+            var tokenValidationParameters = new TokenValidationParameters()
+            {
+                IssuerSigningKey = _securityKey
+            };
+
+            var result = _tokenService.ValidateSignature(token, tokenValidationParameters);
+            if (!result)
+            {
+                return new ResponseDto<UserAccountDto>()
+                {
+                    Error = "Token does not have a valid signature."
+                };
+            }
+
+            var json = JsonConvert.SerializeObject(jwt);
+            foreach (var keyValuePair in payload)
+            {
+                switch (keyValuePair.Key)
+                {
+                    case "username":
+                        username = keyValuePair.Value.ToString();
+                        break;
+                    case "password":
+                        password = keyValuePair.Value.ToString();
+                        break;
+                    case "application":
+                        if (keyValuePair.Value.ToString() != "GetUsGrub")
+                        {
+                            return new ResponseDto<UserAccountDto>()
+                            {
+                                Error = "Request does not match application name."
+                            };
+                        }
+                        break;
+                    case "roleType":
+                        roleType = keyValuePair.Value.ToString();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            using (var authenticationGateway = new AuthenticationGateway())
+            {
+                var gatewayResult = authenticationGateway.StoreSsoToken(new SsoToken(token));
+                if (gatewayResult.Error != null)
+                {
+                    return new ResponseDto<UserAccountDto>()
+                    {
+                        Error = gatewayResult.Error
+                    };
+                }
+            }
+
+            return new ResponseDto<UserAccountDto>()
+            {
+                Data = new UserAccountDto(username: username, password: password, roleType: roleType)
+            };
+        }
+
+        public void Dispose() {}
+    }
+}
--- CSULB.GetUsGrub/CSULB.GetUsGrub.BusinessLogic/Services/TokenService.cs
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.BusinessLogic/Services/TokenService.cs
@@ -1,82 +1,40 @@
-﻿using CSULB.GetUsGrub.Models;
-using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json;
-using System;
+﻿using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
-// TODO: @Jenn compare token to database. Add token to database [-Jenn]
+// TODO: @Jenn Waiting for Ahmed [-Jenn]
 namespace CSULB.GetUsGrub.BusinessLogic
 {
-    public class TokenService : IDisposable
+    public class TokenService
     {
-        private readonly string _ssoSecret = "db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw==";
+        private readonly JwtSecurityTokenHandler _jwtTokenHandler;
 
-        // TODO: @Jenn Should we send back a ResponseDto? May need to encapsulate some procedures in reusable methods [-Jenn]
-        public ResponseDto<UserAccountDto> ValidateToken(string token)
+        public TokenService()
         {
-            string username = "";
-            string password = "";
-            string roleType = "";
-    
-            var jwtTokenHandler = new JwtSecurityTokenHandler();
-            if (jwtTokenHandler.CanReadToken(token) == false)
-            {
-                return new ResponseDto<UserAccountDto>()
-                {
-                    Error = "The token is not a well formed Json Web Token."
-                };
-            }
-            var jwt = jwtTokenHandler.ReadJwtToken(token);
-            var header = jwt.Header;
-            var payload = jwt.Payload;
-            var rawSignature = jwt.RawSignature;
+            _jwtTokenHandler = new JwtSecurityTokenHandler();
+        }
+        
+        // TODO: @Jenn Comment this [-Jenn]
+        public bool CheckIfTokenIsJsonWebToken(string token)
+        {
+            return _jwtTokenHandler.CanReadToken(token);
+        }
 
-            var result = ValidateSignature(_ssoSecret, Base64UrlEncoder.Encode(header.ToString()), Base64UrlEncoder.Encode(payload.ToString()), rawSignature);
-
-            var json = JsonConvert.SerializeObject(jwt);
-            foreach (var keyValuePair in payload)
-            {
-                switch (keyValuePair.Key)
-                {
-                    case "username":
-                        username = keyValuePair.Value.ToString();
-                        break;
-                    case "password":
-                        password = keyValuePair.Value.ToString();
-                        break;
-                    case "application":
-                        if (keyValuePair.Value.ToString() != "GetUsGrub")
-                        {
-                            return new ResponseDto<UserAccountDto>()
-                            {
-                                Error = "Request does not match application name."
-                            };
-                        }
-                        break;
-                    case "roleType":
-                        roleType = keyValuePair.Value.ToString();
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            return new ResponseDto<UserAccountDto>()
-            {
-                Data = new UserAccountDto(username: username, password: password, roleType: roleType)
-            };
+        public JwtSecurityToken GetJwtSecurityToken(string token)
+        {
+            return _jwtTokenHandler.ReadJwtToken(token);
         }
 
         // TODO: @Jenn Comment this [-Jenn]
-        public bool ValidateSignature(string secret, string tokenHeader, string tokenPayload, string rawSignature)
+        public bool ValidateSignature(string token, TokenValidationParameters tokenValidationParameters)
         {
-            var payloadHasher = new PayloadHasher();
-            // TODO: @Jenn Unit Test this encoder [-Jenn]
-            // Verify signature is valid
-            var signature = payloadHasher.Sha256HashWithSalt(salt: secret, payload: tokenHeader + "." + tokenPayload);
+            //var payloadHasher = new PayloadHasher();
+            //// TODO: @Jenn Unit Test this encoder [-Jenn]
+            //// Verify signature is valid
+            //var signature = payloadHasher.Sha256HashWithSalt(salt: secret, payload: tokenHeader + "." + tokenPayload);
 
-            return rawSignature == signature;
+            //return rawSignature == signature;
+            var principal = _jwtTokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securitytoken);
+
+            return true;
         }
-
-        public void Dispose() {}
     }
 }

9be3e3233e655038e9994bb15252d861eb908e2e
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-20 12:49:21 -0700

    Updating SsoController
    50% done. Need to wait for Ahmed to finish the Token Service

--- CSULB.GetUsGrub/CSULB.GetUsGrub/Controllers/SsoController.cs
+++ CSULB.GetUsGrub/CSULB.GetUsGrub/Controllers/SsoController.cs
@@ -1,5 +1,4 @@
 ﻿using CSULB.GetUsGrub.BusinessLogic;
-using CSULB.GetUsGrub.Models;
 using System;
 using System.Net.Http;
 using System.Web.Http;
@@ -15,29 +14,16 @@ namespace CSULB.GetUsGrub
         //[EnableCors(origins: "http://localhost:8080", headers: "*", methods: "POST")]
         public IHttpActionResult RegisterFirstTimeSsoUser(HttpRequestMessage request)
         {
-            var userManager = new UserManager();
-
             try
             {
-                ResponseDto<UserAccountDto> tokenResult;
-
                 // TODO: @Jenn Check for if message has a token. Where to put this? [-Jenn]
                 if (request.Headers.Authorization == null)
                 {
                     return BadRequest("Request does not contain a token.");
                 }
 
-                using (var tokenService = new TokenService())
-                {
-                   tokenResult = tokenService.ValidateToken(request.Headers.Authorization.Parameter);
-                }
+                var result = new SsoTokenManager().ValidateToken(request.Headers.Authorization.Parameter);
 
-                if (tokenResult.Error != null)
-                {
-                    return BadRequest(tokenResult.Error);
-                }
-
-                var result = userManager.CreateFirstTimeSsoUser(tokenResult.Data);
                 if (result.Error != null)
                 {
                     return BadRequest(result.Error);

c72452854c651e90a18fadcb9c84b6b43f7bd23e
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-22 01:34:20 -0700

    Revert "/sso-registration:"
    This reverts commit 2e37ea984d8c0e0d7e7039ccd71c207070a1489c.

--- CSULB.GetUsGrub/CSULB.GetUsGrub.BusinessLogic/Validators/ModelValidators/RestaurantDetailValidator.cs
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.BusinessLogic/Validators/ModelValidators/RestaurantDetailValidator.cs
@@ -1,32 +1,8 @@
-﻿using CSULB.GetUsGrub.Models;
-using FluentValidation;
-using System;
-// TODO: @Jenn Comment this yo and unit test [-Jenn]
-namespace CSULB.GetUsGrub.BusinessLogic
+﻿namespace CSULB.GetUsGrub.BusinessLogic
 {
-    public class RestaurantDetailValidator : AbstractValidator<RestaurantDetail>
+    public class RestaurantDetailValidator
     {
-        public RestaurantDetailValidator()
-        {
-            RuleSet("CreateUser", () =>
-            {
-                RuleFor(x => x.Category)
-                    .NotEmpty().WithMessage("Restaurant category is required.")
-                    .NotNull().WithMessage("Restaurant category is required.");
-
-                RuleFor(x => x.AvgFoodPrice)
-                    .NotEmpty().WithMessage("Average food price is required.")
-                    .Must(x => (x >= 0 && x <= 2)).WithMessage("Average food price is invalid.");
-            });
-        }
         // TODO: @Jenn Validate Category is not empty, not null and in enum [-Jenn]
-        public ResponseDto<bool> CheckIfCategoryIsRestaurantCategory(string category)
-        {
-            // True if day is in RestaurantCategories enum
-            return new ResponseDto<bool>()
-            {
-                Data = Enum.IsDefined(typeof(RestaurantCategories), category.Replace(" ", string.Empty))
-            };
-        }
+        // TODO: @Jenn Validate avgPrice is not empty, not null and in enum [-Jenn]
     }
 }

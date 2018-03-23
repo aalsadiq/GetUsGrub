25729154fa21c7e738c546202e375f3580309354
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-23 09:25:40 -0700

    Updated token service and added token validator unit test

---
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.UnitTests/CSULB.GetUsGrub.BusinessLogic/Validators/TokenValidator.cs
@@ -0,0 +1,21 @@
+﻿using CSULB.GetUsGrub.BusinessLogic;
+using Xunit;
+
+namespace CSULB.GetUsGrub.UnitTests
+{
+    public class TokenValidator
+    {
+        TokenValidator tokenValidator = new TokenValidator();
+
+        [Fact]
+        public void Should_NotThrowException_When_JwtIsValid()
+        {
+            // Arrange
+            var token = "";
+            var key = "";
+            var jwt = new TokenService().GetJwtSecurityToken(token);
+
+            // 
+        }
+    }
+}

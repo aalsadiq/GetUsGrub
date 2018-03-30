7e8c80188f5fee940e078bf9141e236e907e5e83
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-03-30 14:59:31 -0700

    Updated UserClaims model to include ClaimConverter
    Files affected include the configuration file in the UserDbContext

---
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.Models/Converters/ClaimConverter.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Security.Claims;
+
+namespace CSULB.GetUsGrub.Models
+{
+    // TODO: @Jenn Comment this and unit test [-Jenn]
+    class ClaimConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(System.Security.Claims.Claim);
+        }
+
+        public override object ReadJson(JsonReader jsonReader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var jsonObject = JObject.Load(jsonReader);
+
+            var type = (string)jsonObject["Type"];
+            var value = (string)jsonObject["Value"];
+            var valueType = (string)jsonObject["ValueType"];
+            var issuer = (string)jsonObject["Issuer"];
+            var originalIssuer = (string)jsonObject["OriginalIssuer"];
+
+            return new Claim(type: type, value: value, valueType: valueType, issuer: issuer, originalIssuer: originalIssuer);
+
+        }
+
+        public override bool CanWrite => false;
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

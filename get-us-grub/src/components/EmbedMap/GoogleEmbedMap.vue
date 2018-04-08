34ab57fca875444d60f97969ba1a24f300da9349
Brian Fann <fannbrian@gmail.com>
2018-04-08 03:24:57 -0700

    Fixed Google Maps Integration

--- get-us-grub/src/components/EmbedMap/GoogleEmbedMap.vue
+++ get-us-grub/src/components/EmbedMap/GoogleEmbedMap.vue
@@ -1,24 +1,18 @@
 <template>
   <div id="google-embed-map">
-    <app-header />
-    <div id="vue-frame">
-      <!--iframe src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyCfKElVtKARYlgvCdQXBImfjRH5rmUF0mg&amp;origin=Oslo+Norway&amp;destination=Telemark+Norway"></iframe>-->
-      <iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyCfKElVtKARYlgvCdQXBImfjRH5rmUF0mg&amp;origin=Oslo+Norway&amp;destination=Telemark+Norway" allowfullscreen></iframe>
-      <br />
-    </div>
-    <app-footer />
+      <iframe :src="mapUrl" width = 500 height = 500></iframe>
   </div>
 </template>
 
-<script src="/dist/vue-frame.js"></script>
 <script>
-import AppHeader from '@/components/AppHeader'
-import AppFooter from '@/components/AppFooter'
 export default {
   name: 'GoogleEmbedMap',
   components: {
-    'app-header': AppHeader,
-    'app-footer': AppFooter
+  },
+  computed: {
+    mapUrl () {
+      return this.$store.getters.mapUrl
+    }
   }
 }
 </script>

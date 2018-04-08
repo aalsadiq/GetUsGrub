167e919338079a8b25612d3d09910f4d0a1ca555
Unknown <rachel.dang@student.csulb.edu>
2018-04-07 22:40:51 -0700

    Updated ClaimsFactory to follow factory pattern instead of abstract factory pattern

---
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.UserAccessControl/Models/Interfaces/IClaims.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace CSULB.GetUsGrub.UserAccessControl
+{
+    /// <summary>
+    /// Defines the interface for a claims collection object
+    /// 
+    /// @author: Rachel Dang
+    /// @updated: 04/07/18
+    /// </summary>
+    public interface IClaims
+    {
+        string Type { get; }
+        ICollection<Claim> Claims { get; }
+    }
+}

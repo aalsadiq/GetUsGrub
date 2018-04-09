1bf9ec58465b5583a5042c60e0d0c63e72393356
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-04-09 15:57:55 -0700

    Minor changes to the configuration file for foodTypes

--- CSULB.GetUsGrub/CSULB.GetUsGrub.DataAccess/Migrations/UserDbContext/Configuration.cs
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.DataAccess/Migrations/UserDbContext/Configuration.cs
@@ -229,8 +229,8 @@ namespace CSULB.GetUsGrub.DataAccess.Migrations.UserDbContext
                             },
                             Details = new RestaurantDetail()
                             {
-                                // 0 = $0 to $10, 1 = $10.01 to $50, 2 = $50.01+
-                                AvgFoodPrice = randomizer.Next(0, 2 + 1),
+                                // 1 = $0 to $10, 2 = $10.01 to $50, 3 = $50.01+
+                                AvgFoodPrice = randomizer.Next(1, 3 + 1),
                                 FoodType = validFoodTypes[randomizer.Next(0, validFoodTypes.Count)],
                                 HasReservations = true,
                                 HasDelivery = true,

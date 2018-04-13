d35b5bcf1a2bd9f2ca9f3e0ab638548dded21b6d
Ahmed <aalsadiq88@gmail.com>
2018-04-12 21:07:54 -0700

    Getting ready to merge to master

--- CSULB.GetUsGrub/CSULB.GetUsGrub.DataAccess/Contexts/UserContext.cs
+++ CSULB.GetUsGrub/CSULB.GetUsGrub.DataAccess/Contexts/UserContext.cs
@@ -22,7 +22,7 @@ namespace CSULB.GetUsGrub.DataAccess
         public DbSet<AuthenticationToken> AuthenticationTokens { get; set; }
         public DbSet<UserClaims> UserClaims { get; set; }
         public DbSet<SecurityAnswerSalt> SecurityAnswerSalts { get; set; }
-        public DbSet<FoodPreferences> FoodPreferences { get; set; }
+        public DbSet<FoodPreference> FoodPreferences { get; set; }
         public DbSet<FailedAttempts> FailedAttempts { get; set; }
 
         public UserContext() : base("GetUsGrub") { }

10109018b05c7920e2959b099361396ff5b6b87c
Rachel Dang <rachel.dang@student.csulb.edu>
2018-04-15 13:56:52 -0700

    Added front-end for food preferences back in.

---
+++ get-us-grub/src/components/FoodPreferences/FoodPreferences.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <app-header/>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for='preference in foodPreferences' :key='preference'>{{ preference }}</li>
+    </ul>
+    <app-footer/>
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader'
+import AppFooter from '@/components/AppFooter'
+import axios from 'axios'
+
+export default {
+  name: 'FoodPreferences',
+  components: {
+    'app-header': AppHeader,
+    'app-footer': AppFooter
+  },
+
+  data () {
+    return {
+      title: 'Your Food Preferences',
+      foodPreferences: ['Pescetarian', 'Lacto-Vegetarian'],
+      errors: []
+    }
+  },
+
+  created () {
+    axios.get('http://localhost:8081/FoodPreferences').then(response => {
+      this.foodPreferences = response.data
+    }).catch(e => {
+      this.errors.push(e)
+    })
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+    display: inline-block;
+}
+li {
+  list-style-type: circle;
+  text-align: left;
+}
+</style>

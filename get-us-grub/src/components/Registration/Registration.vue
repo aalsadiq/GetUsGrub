63cdc77c97a99cd21321c666ab4ba05c113efd84
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-04-16 21:35:24 -0700

    Add disabled functionality and routing to Registration Vue

--- get-us-grub/src/components/Registration/Registration.vue
+++ get-us-grub/src/components/Registration/Registration.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <app-header/>
-    <app-create-user/>
+    <v-container class="scroll-y" id="scroll-target">
+      <v-container id="registration">
+        <app-create-user/>
+      </v-container>
+    </v-container>
     <app-footer/>
   </div>
 </template>
@@ -17,6 +21,28 @@ export default {
     'app-header': AppHeader,
     'app-footer': AppFooter,
     'app-create-user': AppCreateUser
+  },
+  beforeCreate () {
+    try {
+      if (this.$store.state.authenticationToken === null) {
+      } else {
+        this.$router.push({path: '/'})
+      }
+    } catch (ex) {
+      this.$router.push({path: '/'})
+    }
   }
 }
 </script>
+
+<style>
+#scroll-target {
+  max-height: 50.5em;
+  margin: -0.8em 1em 0em 1em;
+}
+#registration {
+  max-width: 1200px;
+  padding-top: -2em;
+  margin: -2em 0em 0em 7%;
+}
+</style>

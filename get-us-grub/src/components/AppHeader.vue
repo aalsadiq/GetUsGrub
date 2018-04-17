3c138623187ea157ff1a7cff79cf0031f0b63dc9
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-04-16 21:38:52 -0700

    Hide/show nav-bar buttons depending on user's permissions

--- get-us-grub/src/components/AppHeader.vue
+++ get-us-grub/src/components/AppHeader.vue
@@ -1,31 +1,113 @@
 <template>
     <v-toolbar id="header-toolbar" dark fixed>
-      <div>
+      <div v-show="!showRestaurantSelection()">
         <router-link to="/">
-        <v-btn flat class="home-btn">
-          <v-icon>home</v-icon>
-          <v-toolbar-title>
-            <span id="toolbar-title">
-              GetUsGrub
-            </span>
-          </v-toolbar-title>
-        </v-btn>
-      </router-link>
+          <v-btn flat class="home-btn">
+            <v-icon>home</v-icon>
+            <v-toolbar-title>
+              <span id="toolbar-title">
+                GetUsGrub
+              </span>
+            </v-toolbar-title>
+          </v-btn>
+        </router-link>
+      </div>
+      <div v-show="showRestaurantSelection()">
+        <router-link to="/RestaurantSelection/Registered">
+          <v-btn flat class="home-btn">
+            <v-icon>home</v-icon>
+            <v-toolbar-title>
+              <span id="toolbar-title">
+                GetUsGrub
+              </span>
+            </v-toolbar-title>
+          </v-btn>
+        </router-link>
       </div>
       <v-spacer></v-spacer>
       <div>
         <router-link to="/Registration">
-          <v-btn color="red lighten-3" class="nav-btn"><span class="btn-text">REGISTER</span></v-btn>
+          <v-btn
+            color="red lighten-3"
+            class="nav-btn"
+            v-show="showWithoutAuthentication()"
+          >
+            <span class="btn-text">REGISTER</span>
+          </v-btn>
         </router-link>
         <router-link to="/Login">
-          <v-btn color="red lighten-3" class="nav-btn"><span class="btn-text">LOGIN</span></v-btn>
+          <v-btn
+            color="red lighten-3"
+            class="nav-btn"
+            v-show="showWithoutAuthentication()"
+          >
+            <span class="btn-text">LOGIN</span>
+          </v-btn>
         </router-link>
         <router-link to="/RestaurantBillSplitter">
-          <v-btn color="red lighten-3" class="nav-btn"><span class="btn-text">SPLIT BILL</span></v-btn>
+          <v-btn
+            color="red lighten-3"
+            class="nav-btn"
+          >
+            <span class="btn-text">SPLIT BILL</span>
+          </v-btn>
+        </router-link>
+        <router-link to="/Profile">
+          <v-btn
+            color="red lighten-3"
+            class="nav-btn"
+          >
+            <span class="btn-text">PROFILE</span>
+          </v-btn>
+        </router-link>
+        <router-link to="/Logout">
+          <v-btn
+            color="grey"
+            class="nav-btn"
+            v-show="!showWithoutAuthentication()"
+          >
+            <span class="btn-text">LOGOUT</span>
+          </v-btn>
         </router-link>
       </div>
     </v-toolbar>
 </template>
+
+<script>
+import jwt from 'jsonwebtoken'
+
+export default {
+  data () {
+    return {
+      showRegisteredRestaurantSelection: false
+    }
+  },
+  methods: {
+    showRestaurantSelection () {
+      try {
+        if (jwt.decode(this.$store.state.authenticationToken).ReadRestaurantSelection === 'True') {
+          return true
+        } else {
+          return false
+        }
+      } catch (ex) {
+        return false
+      }
+    },
+    showWithoutAuthentication () {
+      try {
+        if (this.$store.state.authenticationToken === null) {
+          return true
+        } else {
+          return false
+        }
+      } catch (ex) {
+        return false
+      }
+    }
+  }
+}
+</script>
 
 <style>
 #nav {

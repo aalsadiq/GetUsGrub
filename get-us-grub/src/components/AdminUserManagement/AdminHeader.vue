a4d515edd3ebacba6cc41af7a0505123717c01be
Jenn <Jennifer.Nguyen27@student.csulb.edu>
2018-04-17 17:20:47 -0700

    Updated Admin Vues with Angelica

--- get-us-grub/src/components/AdminUserManagement/AdminHeader.vue
+++ get-us-grub/src/components/AdminUserManagement/AdminHeader.vue
@@ -1,14 +1,14 @@
 <template>
 <div>
-  <!-- <v-card height="350px"> -->
-        <!-- <v-navigation-drawer id="menu" stateless hide-overlay :mini-variant.sync="mini" v-model="drawer"> -->
-    <v-navigation-drawer permanent absolute v-model="drawer" >
+    <!-- <v-card height="350px"> -->
+    <!-- <v-navigation-drawer id="menu" stateless hide-overlay :mini-variant.sync="mini" v-model="drawer"> -->
+    <v-navigation-drawer id="nav-drawer" permanent absolute v-model="drawer" >
       <!-- used to navigate through application to work with or without vue-router outside the box-->
       <!--permanent: remains visible regardless of screensize absolute: Position the element absolutely is false v-model= drawer-->
       <!--v-toolbar:flat: removes the toolbar box-shadow-->
       <v-toolbar flat class="transparent"> <!-- the square to the left -->
       <!--v-list: where our items are held..-->
-        <!-- <v-list class="pa-0"> -->
+        <v-list class="pa-0">
           <v-list-tile avatar> <!--avatar: used to set minimum tile height on a single-line list item -->
             <v-list-tile-avatar>
               <img src="@/assets/ProfileImages/DefaultProfileImage.png"><!--Grab image from the store... -->
@@ -18,7 +18,7 @@
               <v-list-tile-title>Admin admin</v-list-tile-title><!-- Grab username when logged in?-->
             </v-list-tile-content>
           </v-list-tile>
-        <!-- </v-list> -->
+        </v-list>
       </v-toolbar>
       <v-list class="header-admin" dense>
         <v-list-tile v-for="item in items" :key="item.title" @click="item" :to="item.path">
@@ -67,7 +67,6 @@ margin-right:0px;
 margin-top:0px;
 width:0px;
 height:0px;
-style:0px;
 padding:0px;
 }
 </style>
